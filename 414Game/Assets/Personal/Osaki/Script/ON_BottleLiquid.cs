32bc1b90592687d4ec98adc949442a1669737210
noriaki222 <osakinoriaki222@gmail.com>
2023-03-06 10:46:49 +0900

    Create Shader and Script

---
+++ 414Game/Assets/Personal/Osaki/Script/ON_BottleLiquid.cs
@@ -0,0 +1,26 @@
+/**
+ * @file ON_BottleLiquid.cs
+ * @brief 液体がモデルに入ってるようにマテリアルを変更するスクリプト
+ * @author Osaki Noriaki
+ * @date 2023/03/06
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ON_BottleLiquid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
