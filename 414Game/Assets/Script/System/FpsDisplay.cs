30f5f7bc73a5c41f62c41e0bfd42bfe97b8e5444
Kanase <pengingame2@gmail.com>
2023-03-10 00:19:45 +0900

    Create FPS
    Create FPS Viewer&Fixed

---
+++ 414Game/Assets/Script/System/FpsDisplay.cs
@@ -0,0 +1,46 @@
+﻿/**
+ * @file FpsDisplay.cs
+ * @brief FPS値の固定及び表示
+ * @author 吉田叶聖
+ * @date 2023/03/10
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsDisplay : MonoBehaviour
+{
+
+    // 変数
+    int frameCount;
+    float prevTime;
+    float fps;
+
+    // 初期化処理
+    void Start()
+    {
+        frameCount = 0;
+        prevTime = 0.0f;
+    }
+    // 更新処理
+    void Update()
+    {
+        Application.targetFrameRate = 60;
+        frameCount++;
+        float time = Time.realtimeSinceStartup - prevTime;
+
+        if (time >= 0.5f)
+        {
+            fps = frameCount / time;
+
+            frameCount = 0;
+            prevTime = Time.realtimeSinceStartup;
+        }
+    }
+    private void OnGUI()
+    {
+        GUI.color = Color.black;
+        GUI.skin.label.fontSize = 100;
+        GUILayout.Label(fps.ToString());
+    }
+}
