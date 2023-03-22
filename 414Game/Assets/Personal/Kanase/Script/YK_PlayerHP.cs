98b23bddda61bf7c5d4ae082aaaf7751a404e5c0
Kanase <pengingame2@gmail.com>
2023-03-22 16:33:47 +0900

    Fixed HPBarVIsible&Canvas
    HPバーのサイズ調整完了
    バーの赤い方が後から追うように消えるのはまだ未完成

--- 414Game/Assets/Personal/Kanase/Script/YK_PlayerHP.cs
+++ 414Game/Assets/Personal/Kanase/Script/YK_PlayerHP.cs
@@ -1,7 +1,7 @@
-/**
+﻿/**
  * @file PlayerHP.cs
- * @brief �v���C���[�̗̑�
- * @author �g�c����
+ * @brief プレイヤーの体力
+ * @author 吉田叶聖
  * @date 2023/03/06
  */
 using System.Collections;
@@ -11,54 +11,108 @@ using UnityEngine.UI;
 
 public class YK_PlayerHP : MonoBehaviour
 {
-    // �̗͒l���i�[����ϐ��i��U 100�j
-    public static int nMaxHP = 100;
-    // ���݂̗̑͒l���i�[����ϐ��i�����l�� maxHealth�j
-    public int nCurrentHP = nMaxHP;
-    //Slider������
-    [SerializeField] Slider HPBar;
-    // Start is called before the first frame update
+
+    //　最大HP
+    [SerializeField]
+    private int m_nMaxHP = 10000;
+    //　徐々に減らしていくhp計測に使う
+    [SerializeField]
+    private int m_nHP;
+    //　最終的なhp計測に使う
+    private int m_nFinalHP;
+    //　HPを一度減らしてからの経過時間
+    private float m_fCountLime = 0f;
+    //　次にHPを減らすまでの時間
+    [SerializeField]
+    private float m_fNextCountTime = 0f;
+    //　HP表示用スライダー
+    [SerializeField]
+    private Slider HpSlider;
+    //　一括HP表示用スライダー
+    [SerializeField]
+    private Slider BulkHPSlider;
+    //　現在のダメージ量
+    private int m_nDamage = 0;
+    //　一回に減らすダメージ量
+    [SerializeField]
+    private int m_nAmountOfDamageAtOneTime = 100;
+    //　HPを減らしているかどうか
+    private bool m_bReducing;
+    //　HP用表示スライダーを減らすまでの待機時間
+    [SerializeField]
+    private float m_fDelayTime = 1f;
+
     void Start()
     {
-        //Slider�𖞃^���ɂ���B
-        HPBar.value = 1;
+        m_nHP = m_nMaxHP;
+        m_nFinalHP = m_nMaxHP;
+        HpSlider.value = 1;
+        BulkHPSlider.value = 1;
     }
 
-    // Update is called once per frame
     void Update()
     {
-       
-        //�ő�HP�ɂ����錻�݂�HP��Slider�ɔ��f�B
-        //int���m�̊���Z�͏����_�ȉ���0�ɂȂ�̂ŁA
-        //(float)������float�̕ϐ��Ƃ��ĐU���킹��B
-        HPBar.value = (float)nCurrentHP / (float)nMaxHP;
+        //　ダメージなければ何もしない
+        if (!m_bReducing)
+        {
+            return;
+        }
+        //　次に減らす時間がきたら
+        if (m_fCountLime >= m_fNextCountTime)
+        {
+            int tempDamage;
+            //　決められた量よりも残りダメージ量が小さければ小さい方を1回のダメージに設定
+            tempDamage = Mathf.Min(m_nAmountOfDamageAtOneTime, m_nDamage);
+            m_nHP -= tempDamage;
+            //　全体の比率を求める
+            HpSlider.value = (float)m_nHP / m_nMaxHP;
+            //　全ダメージ量から1回で減らしたダメージ量を減らす
+            m_nDamage -= tempDamage;
+            //　全ダメージ量が0より下になったら0を設定
+            m_nDamage = Mathf.Max(m_nDamage, 0);
+
+            m_fCountLime = 0f;
+            //　ダメージがなくなったらHPバーの変更処理をしないようにする
+            if (m_nDamage <= 0)
+            {
+                m_bReducing = false;
+            }
+
+            //　HPが0以下になったら敵を削除
+            if (m_nHP <= 0)
+            {
+                Debug.Log("ゲームオーバー");
+            }
+        }
+        m_fCountLime += Time.deltaTime;
     }
-    // �_���[�W����
+
+    //　ダメージ値を追加するメソッド
     public void DelLife(int damage)
     {
-        // ���݂̗̑͒l���� ���� damage �̒l������
-        nCurrentHP -= damage;
-        // ���݂̗̑͒l�� 0 �ȉ��̏ꍇ
-        if (nCurrentHP <= 0)
-        {
-            // ���݂̗̑͒l�� 0 ����
-            nCurrentHP = 0;
-            // �R���\�[����"Dead!"��\������
-            Debug.Log("Dead!");
-        }
+        //　ダメージを受けた時に一括HP用のバーの値を変更する
+        var tempHP = Mathf.Max(m_nFinalHP -= damage, 0);
+        BulkHPSlider.value = (float)tempHP / m_nMaxHP;
+        this.m_nDamage += damage;
+        m_fCountLime = 0f;
+        //　一定時間後にHPバーを減らすフラグを設定
+        Invoke("StartReduceHP", m_fDelayTime);
     }
-    // �񕜏���
-    public void AddLife(int heal)
+    // ダメージ処理
+    // 回復処理
+    public void AddLife(int damege)
     {
-        // ���݂̗̑͒l���� ���� heal �̒l�𑫂�
-        nCurrentHP += heal;
-        // ���݂̗̑͒l�� maxHealth �ȏ�̏ꍇ
-        if (nCurrentHP >= nMaxHP)
-        {
-            // ���݂̗̑͒l�� �ő�l ����
-            nCurrentHP = nMaxHP;
-            // �R���\�[����"Max!"��\������
-            Debug.Log("Max!");
-        }
+        //　ダメージを受けた時に一括HP用のバーの値を変更する
+        var tempHP = Mathf.Max(m_nFinalHP += m_nDamage, 0);
+        BulkHPSlider.value = (float)tempHP / m_nMaxHP;
+        this.m_nDamage -= m_nDamage;
+        m_fCountLime = 0f;
+        //　一定時間後にHPバーを減らすフラグを設定
+        Invoke("StartReduceHP", m_fDelayTime);
+    }
+    //　徐々にHPバーを減らすのをスタート
+    public void StartReduceHP()
+    {
+        m_bReducing = true;
     }
 }
