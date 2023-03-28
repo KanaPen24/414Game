0fd5e3a1607979c2a27564465faf7be6c3f2742b
Kanase <pengingame2@gmail.com>
2023-03-29 03:39:00 +0900

    Create HandAnim
    もっとこうしてという意見を求めます

--- 414Game/Assets/Personal/Ihara/Script/Player/IS_Player.cs
+++ 414Game/Assets/Personal/Ihara/Script/Player/IS_Player.cs
@@ -63,6 +63,7 @@ public class IS_Player : MonoBehaviour
     [SerializeField] private Rigidbody               m_Rigidbody;        // PlayerのRigidBody
     [SerializeField] private YK_HPBarVisible         m_HpVisible;        // PlayerのHp表示管理
     [SerializeField] private YK_PlayerHP             m_Hp;               // PlayerのHp
+    [SerializeField] private YK_UICatcher            m_UICatcher;        // UIキャッチャー
     [SerializeField] private List<IS_PlayerStrategy> m_PlayerStrategys;  // Player挙動クラスの動的配列
     [SerializeField] private List<IS_Weapon>         m_Weapons;          // 武器クラスの動的配列
     [SerializeField] private PlayerState             m_PlayerState;      // Playerの状態を管理する
@@ -141,9 +142,16 @@ public class IS_Player : MonoBehaviour
 
         // Decision=Key.Z,Joy.A
         if (Input.GetButtonDown("Decision"))
-        {
-            m_HpVisible.GetSetVisible = !m_HpVisible.GetSetVisible;
-            m_bEquipFlg = !m_HpVisible.GetSetVisible;
+        {            
+            if(m_bEquipFlg)
+            {
+                m_HpVisible.GetSetVisible = true;
+                m_bEquipFlg = false;
+            }
+            else
+            {
+                m_UICatcher.ParticlePlay();
+            }
         }
 
         // 武器チェンジ(仮)…関数化する予定
