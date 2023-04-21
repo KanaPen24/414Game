d875239b2c8e0b308390cd6ac9136454fffbaa2a
Kanase <pengingame2@gmail.com>
2023-04-21 13:51:59 +0900

    Revert "UI Change"
    This reverts commit 5dc82c579a0c4b15864a278cbc5b5216dd4f5305.

--- 414Game/Assets/Personal/Kanase/Script/YK_HPBar.cs
+++ 414Game/Assets/Personal/Kanase/Script/YK_HPBar.cs
@@ -14,8 +14,10 @@ public class YK_HPBar : YK_UI
     [SerializeField] Slider HP;
     [SerializeField] private Image FrontFill;    //バーの表面のテクスチャ
     [SerializeField] private Image BackFill;     //後ろのバーの表面のテクスチャ
+    [SerializeField] private Image BackGround;   //バーの裏のテクスチャ
     [SerializeField] private Image Frame;        //フレーム
     [SerializeField] private YK_Hand m_Hand;
+    [SerializeField] private IS_WeaponHPBar weaponHpBar;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,22 @@ public class YK_HPBar : YK_UI
         GetSetScale = HP.transform.localScale;
     }
 
+    private void Update()
+    {
+        // ※ここで画像の切替を行う
+        if(weaponHpBar.GetSetHp <= 25)
+        {
+
+        }
+        else if (weaponHpBar.GetSetHp <= 50)
+        {
+
+        }
+        else if(weaponHpBar.GetSetHp <= 75)
+        {
+
+        }
+    }
 
     public override void UIFadeIN()
     {
@@ -38,7 +56,8 @@ public class YK_HPBar : YK_UI
         // 1秒でテクスチャをフェードイン
         FrontFill.DOFade(1f, 0f);
         BackFill.DOFade(1f, 0f);
-        Frame.DOFade(1f, 0f).OnComplete(() =>
+        Frame.DOFade(1f, 0f);
+        BackGround.DOFade(1f, 0f).OnComplete(() =>
         {
             GetSetFadeState = FadeState.FadeNone;
             m_Hand.HandPull();
@@ -54,7 +73,8 @@ public class YK_HPBar : YK_UI
         // 1秒でテクスチャをフェードアウト
         FrontFill.DOFade(0f, 1f);
         BackFill.DOFade(0f, 1f);
-        Frame.DOFade(0f, 1f).OnComplete(() =>
+        Frame.DOFade(0f, 1f);
+        BackGround.DOFade(0f, 1f).OnComplete(() =>
         {
             GetSetFadeState = FadeState.FadeNone;
             Debug.Log("FadeOUT終了");
