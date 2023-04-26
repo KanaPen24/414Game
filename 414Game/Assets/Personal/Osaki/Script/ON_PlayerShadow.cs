11cc2eae47e63edfb51caade801eb01f79a33e42
noriaki222 <osakinoriaki222@gmail.com>
2023-04-26 17:36:51 +0900

    update:playershadow

--- 414Game/Assets/Personal/Osaki/Script/ON_PlayerShadow.cs
+++ 414Game/Assets/Personal/Osaki/Script/ON_PlayerShadow.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
+﻿/**
+ * @file ON_PlayerShadow.cs
+ * @brief プレイヤーの足元にある影
+ * @author Osaki Noriaki
+ * @date 2023/04/26
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ON_PlayerShadow : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private GameObject target;
+    [SerializeField] private IS_Player target;  // プレイヤー
+    [SerializeField] private GameObject ground; // 地面
     void Start()
     {
         
@@ -14,9 +21,12 @@ public class ON_PlayerShadow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var pos = target.transform.position;
-       // pos.y = transform.position.y;
-        pos.z = transform.position.z;
-        transform.position = pos;
+        var state = target.GetSetPlayerState;
+        if (state != PlayerState.PlayerJump && state != PlayerState.PlayerDrop)
+        {
+            var pos = target.gameObject.transform.position;
+            pos.z = transform.position.z;
+            transform.position = pos;
+        }
     }
 }
