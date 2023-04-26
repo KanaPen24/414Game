56b8436a195d3c9c1a947cec41f7a29fd5dea649
noriaki222 <osakinoriaki222@gmail.com>
2023-04-26 15:14:01 +0900

    create:playershadow

---
+++ 414Game/Assets/Personal/Osaki/Script/ON_PlayerShadow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ON_PlayerShadow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = target.transform.position;
+       // pos.y = transform.position.y;
+        pos.z = transform.position.z;
+        transform.position = pos;
+    }
+}
