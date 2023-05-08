96a926e86dbd67b70f12edf8e9f986fa1763c2fe
naitoo0205 <100120605+naitoo0205@users.noreply.github.com>
2023-05-08 15:17:24 +0900

    ProtoBat Create

--- 414Game/Assets/Personal/Naito/scripts/Bat/NK_Bat_Wait.cs
+++ 414Game/Assets/Personal/Naito/scripts/Bat/NK_Bat_Wait.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class NK_Bat_Wait : NK_BatStrategy
 {
+    [SerializeField] private NK_Bat m_Bat;
+    [SerializeField] private float m_MoveTime;
+    private float m_fCnt;
     public override void UpdateStrategy()
     {
-        
+        m_fCnt += Time.deltaTime;
+        if (m_fCnt > m_MoveTime)
+        {
+            m_fCnt = 0.0f;
+            m_Bat.GetSetBatState = BatState.BatMove;
+        }
     }
 }
