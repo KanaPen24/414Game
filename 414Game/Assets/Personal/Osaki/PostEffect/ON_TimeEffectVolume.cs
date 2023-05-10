a2a7db591a9575ac1f5cee68715e32abc78eb50f
noriaki222 <osakinoriaki222@gmail.com>
2023-05-10 13:45:25 +0900

    create:timePostEffect

---
+++ 414Game/Assets/Personal/Osaki/PostEffect/ON_TimeEffectVolume.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[Serializable]
+[VolumeComponentMenu("Time Effect")]
+public class ON_TimeEffectVolume : VolumeComponent
+{
+    public bool IsActive() => tintColor != Color.white;
+
+    public ColorParameter tintColor = new ColorParameter(Color.white);
+}
---
+++ 414Game/Assets/Personal/Osaki/PostEffect/ON_TimePostEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ON_TimePostEffect : MonoBehaviour
+{
+    private Volume _volume;
+    private ON_TimeEffectVolume timeeffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _volume = GetComponent<Volume>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            ChangeTimePostEffect(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ChangeTimePostEffect(false);
+        }
+    }
+
+    public void ChangeTimePostEffect(bool flg)
+    {
+        if(_volume.profile.TryGet<ON_TimeEffectVolume>(out timeeffect))
+        {
+            timeeffect.active = flg;
+        }
+    }
+}
