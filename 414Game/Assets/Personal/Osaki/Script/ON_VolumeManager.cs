18e3bc006b3c7b0e30dedf545232cb2bde674ea7
noriaki222 <osakinoriaki222@gmail.com>
2023-05-12 16:24:12 +0900

    update:volume

--- 414Game/Assets/Personal/Osaki/Script/ON_VolumeManager.cs
+++ 414Game/Assets/Personal/Osaki/Script/ON_VolumeManager.cs
@@ -7,16 +7,30 @@ public class ON_VolumeManager : MonoBehaviour
 {
     private Volume _volume;
     private ON_TimeEffectVolume timeeffect;
-    private ON_BraunTubeVolume braumTube;
+    private ON_BraunTubeVolume braunTube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _volume = GetComponent<Volume>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // タイマーエフェクトの切り替え
+    public void ChangeTimePostEffect(bool flg)
     {
-        
+        if (_volume.profile.TryGet<ON_TimeEffectVolume>(out timeeffect))
+        {
+            timeeffect.active = flg;
+        }
+    }
+
+    // ブラウン管の遷移
+    public void SetBraunRate(float rate)
+    {
+        if(_volume.profile.TryGet<ON_BraunTubeVolume>(out braunTube))
+        {
+            rate = Mathf.Max(rate, 1);
+            rate = Mathf.Min(rate, 0);
+            braunTube.rate.value = rate;
+        }
     }
 }
