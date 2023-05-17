f898d2baad54ba71be14eb3efbe785155a44edb7
naitoo0205 <100120605+naitoo0205@users.noreply.github.com>
2023-05-17 13:06:39 +0900

    BossCamera

---
+++ 414Game/Assets/Personal/Naito/scripts/BossMole/NK_BossMole_Rush.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NK_BossMole_Rush : NK_BossMoleStrategy
+{
+    [SerializeField] private NK_BossMole m_BossMole;
+
+    public override void UpdateStrategy()
+    {
+        
+    }
+}
