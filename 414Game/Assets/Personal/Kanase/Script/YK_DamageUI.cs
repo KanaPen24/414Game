682dcfa35fd8d15b1b81f4e0cf56eaea9d9d13d6
Kanase <pengingame2@gmail.com>
2023-05-20 00:23:20 +0900

    Fix DamageUI
    時計の時間を回復させれるようになった

--- 414Game/Assets/Personal/Kanase/Script/YK_DamageUI.cs
+++ 414Game/Assets/Personal/Kanase/Script/YK_DamageUI.cs
@@ -17,12 +17,13 @@ public class YK_DamageUI : YK_UI
     //　フェードアウトするスピード
     private float fadeOutSpeed = 1f;
     //　移動値
-    [SerializeField]
-    private float moveSpeed = 0.4f;
-    private int m_nDamage;    
+    [SerializeField] private float moveSpeed = 0.4f;
+    [SerializeField] private int m_nDamage;    
     [SerializeField] private Vector3 m_MinScale = new Vector3(0.5f, 0.5f, 0.5f); // 最小サイズ
-    [SerializeField] private float m_fDelTime = 0.5f; // 減算していく時間
-
+    [SerializeField] private float m_fDelTime = 0.5f;   // 減算していく時間
+    [SerializeField] private int m_nCountDown;          // 消えるまでの時間（秒単位）
+    private int m_nCountTime = 0;                       // 表示されている時間
+    [SerializeField] YK_Time time;                      // 時間
     void Start()
     {        
         damageText = GetComponentInChildren<Text>();
@@ -32,19 +33,24 @@ public class YK_DamageUI : YK_UI
         GetSetPos = this.GetComponent<RectTransform>().anchoredPosition;
         //スケール取得
         GetSetScale = damageText.transform.localScale;
+        m_nCountDown *= 60; //60FPSに合わせる
+        time= GameObject.Find("Timer").GetComponent<YK_Time>();
     }
 
     void LateUpdate()
     {
-       
-        transform.rotation = Camera.main.transform.rotation;
-        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-
-        damageText.color = Color.Lerp(damageText.color, new Color(1f, 0f, 0f, 0f), fadeOutSpeed * Time.deltaTime);
-
-        if (damageText.color.a <= 0.1f)
+        m_nCountTime++;
+        if (m_nCountTime >= m_nCountDown)
         {
-            Destroy(gameObject);
+            transform.rotation = Camera.main.transform.rotation;
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+            damageText.color = Color.Lerp(damageText.color, new Color(1f, 0f, 0f, 0f), fadeOutSpeed * Time.deltaTime);
+
+            if (damageText.color.a <= 0.1f)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
@@ -54,10 +60,11 @@ public class YK_DamageUI : YK_UI
         //damageTextを更新
         damageText.text = "" + damage;
         m_nDamage = damage;
-        Debug.Log(damage);
     }
     public override void UIFadeOUT()
     {
+        //時間をプラスする
+        time.AddTime(m_nDamage);
         m_eFadeState = FadeState.FadeOUT;
         // m_fDelTime秒でm_MinScaleに変更
         damageText.transform.DOScale(m_MinScale, m_fDelTime);
@@ -65,6 +72,7 @@ public class YK_DamageUI : YK_UI
         damageText.DOFade(0f, m_fDelTime).OnComplete(() =>
         {
             GetSetFadeState = FadeState.FadeNone;
+            Destroy(gameObject);
         });
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -77,5 +85,15 @@ public class YK_DamageUI : YK_UI
     private void OnTriggerExit2D(Collider2D collision)
     {
         damageText.color = Color.red;
+    }
+    /**
+* @fn
+* ダメージのgetter
+* @return m_nDamage(int)
+* @brief ダメージを受け取る
+*/
+    public int GetDamage
+    {
+        get { return m_nDamage; }
     }
 }
