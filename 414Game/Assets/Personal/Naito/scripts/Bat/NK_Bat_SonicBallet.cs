a87e0edc42781737a3861b42877db5998adb6350
naitoo0205 <100120605+naitoo0205@users.noreply.github.com>
2023-05-26 13:13:17 +0900

    BatUpdate

--- 414Game/Assets/Personal/Naito/scripts/Bat/NK_Bat_SonicBallet.cs
+++ 414Game/Assets/Personal/Naito/scripts/Bat/NK_Bat_SonicBallet.cs
@@ -6,9 +6,15 @@ public class NK_Bat_SonicBallet : MonoBehaviour
 {
     [SerializeField] private float m_SonicSpeed;
     [SerializeField] private IS_Player m_Player;
+    [SerializeField] private YK_Clock m_Clock;
+    [SerializeField] private ParticleSystem m_DestroyEffect;
 
     private void FixedUpdate()
     {
+        if(m_Clock.GetSetStopTime)
+        {
+            return;
+        }
         this.gameObject.transform.Translate(m_SonicSpeed * Time.deltaTime, 0, 0);
     }
 
@@ -17,6 +23,10 @@ public class NK_Bat_SonicBallet : MonoBehaviour
         if (other.gameObject == m_Player.gameObject)
         {
             m_Player.Damage(15, 1.5f);
+            ParticleSystem Effect = Instantiate(m_DestroyEffect);
+            Effect.Play();
+            Effect.transform.position = this.transform.position;
+            Destroy(Effect.gameObject, 2.0f);
             Destroy(this.gameObject);
         }
         
@@ -26,6 +36,10 @@ public class NK_Bat_SonicBallet : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Floor"))
         {
+            ParticleSystem Effect = Instantiate(m_DestroyEffect);
+            Effect.Play();
+            Effect.transform.position = this.transform.position;
+            Destroy(Effect.gameObject, 2.0f);
             Destroy(this.gameObject);
         }
         
