3a561ed16baa3edadebf4dd96d773681c8e65a86
Kanase <pengingame2@gmail.com>
2023-05-31 02:21:41 +0900

    Create PLPlayEffect
    プレイヤーのエフェクトを再生するためのスクリプト

---
+++ 414Game/Assets/Personal/Kanase/Script/YK_PLPlayEff.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * @file YK_PLPlayEff
+ * @brief プレイヤーの状態でどのエフェクトを流すか
+ * @author 吉田叶聖
+ * @date 2023/05/31
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YK_PLPlayEff : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem AtkEff; //攻撃エフェクト
+    [SerializeField] private ParticleSystem AvoEff; //回避エフェクト
+    [SerializeField] private IS_Player Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //プレイヤーがどういう時にどのエフェクトを流すかのやつ
+        switch(Player.GetSetPlayerState)
+        {
+            case PlayerState.PlayerAttack01:
+            case PlayerState.PlayerAttack02:
+                AtkEff.Play();
+                break;
+            case PlayerState.PlayerAvoidance:
+                AvoEff.Play();
+                break;
+            default:
+                AvoEff.Stop();
+                AvoEff.Stop();
+                break;
+        }
+    }
+}
