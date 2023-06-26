1012b8fa18a81b143508bbec78785c40bf8b8318
Kanase <pengingame2@gmail.com>
2023-06-26 13:47:04 +0900

    Fix Save
    セーブ機能

--- 414Game/Assets/Personal/Kanase/Script/YK_JsonSave.cs
+++ 414Game/Assets/Personal/Kanase/Script/YK_JsonSave.cs
@@ -8,28 +8,36 @@ public class YK_JsonSave : MonoBehaviour
     [HideInInspector] public SaveData data;     // json変換するデータのクラス
     string filepath;                            // jsonファイルのパス
     string fileName = "SaveData.json";          // jsonファイル名
-    
+    public List<IS_Weapon> WeaponHp;            // 保存するための武器の耐久値
+    private int m_nDataLimit;  // データの限界地
     //-------------------------------------------------------------------
     // 開始時にファイルチェック、読み込み
     void Awake()
     {
         // パス名取得
         filepath = Application.dataPath + "/" + fileName;
-       
+
+        m_nDataLimit = WeaponHp.Capacity - 1;//配列のため-１をして調整
+
         // ファイルがないとき、ファイル作成
         if (!File.Exists(filepath))
         {
             Save(data);
         }
 
-        // ファイルを読み込んでdataに格納
-        data = Load(filepath);
+       //ファイルを読み込んでdataに格納
+       data = Load(filepath);
     }
 
     //-------------------------------------------------------------------
     // jsonとしてデータを保存
     public void Save(SaveData data)
     {
+        for (int i = 0; i <= m_nDataLimit; i++)
+        {
+            //data.WeaponHp.Add(WeaponHp[i].GetSetHp);
+            data.WeaponHp[i] = WeaponHp[i].GetSetHp;
+        }
         data.pos = this.gameObject.transform.position;          // dataの座標保存
         string json = JsonUtility.ToJson(data);                 // jsonとして変換
         StreamWriter wr = new StreamWriter(filepath, false);    // ファイル書き込み指定
