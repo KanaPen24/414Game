578d32a5bbc3775cd12d3f2a3f954d5c700167dc
Kanase <pengingame2@gmail.com>
2023-06-30 14:20:42 +0900

    Add Comment
    コメント追記

--- 414Game/Assets/Personal/Kanase/Script/SaveData.cs
+++ 414Game/Assets/Personal/Kanase/Script/SaveData.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/**
+ * @file   SaveData.cs
+ * @brief  何をセーブするか定義する場所
+ * @author 吉田叶聖
+ * @date   2023/06/20
+ */
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
